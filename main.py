631a3eb86172bafbb1f01c4e8424dbc371a9c0bf
Italo Santos <italofsan@outlook.com>
2025-09-10 19:06:05 -0300

    Feat: Display options

---
+++ main.py
@@ -0,0 +1,18 @@
+contact_list = []
+
+while True:
+    print("\nContact list menu")
+    print("1. Add a contact")
+    print("2. Show all contacts")
+    print("3. Edit a contact")
+    print("4. Favorite a contact")
+    print("5. Show all favorite contacts")
+    print("6. Delete a contact")
+    print("7. Exit")
+
+    option = input("\nChoose your option: ")
+
+    if option == "7":
+        break
+
+print("\nContact list closed!")
